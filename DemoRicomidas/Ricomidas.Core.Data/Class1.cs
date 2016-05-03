2da62198916b24ced979b44c3c46ff80bf69ced9
riohacha <riohacha0308@hotmail.com>
2016-05-02 19:51:14 -0500

    Se crea tabla productos

--- DemoRicomidas/Ricomidas.Core.Data/Class1.cs
+++ DemoRicomidas/Ricomidas.Core.Data/Class1.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Ricomidas.Core.Data
 {
-    
+
     //Buscar sobre EntityFramework Code First
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Cantidad { get; set; }
+    }
+
+    
+
 
 }
