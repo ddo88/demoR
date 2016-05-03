3ae8ffdfec1926e9b3c83b995a9e11f5ba59ba6f
Daniel D <ddo88@hotmail.com>
2016-05-02 19:46:16 -0500

    cambios entity

--- DemoRicomidas/Ricomidas.Core.Data/Class1.cs
+++ DemoRicomidas/Ricomidas.Core.Data/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,35 @@ namespace Ricomidas.Core.Data
 {
     
     //Buscar sobre EntityFramework Code First
+
+    public class Contexto : DbContext
+    {
+        public Contexto():base("Model=BaseDatos")
+        {
+            
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+
+    //public class Test
+    //{
+    //    public Test()
+    //    {
+    //        new Contexto().Clientes.Where(x => x.Nombre == "Pepito");
+    //    }
+    //}
+
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Nit { get; set; }
+        public string Telefono { get; set; }
+
+    }
+
+
 
 }
