e4698bc36b25ac75267094e42fb816596a864f66
riohacha <riohacha0308@hotmail.com>
2016-05-04 10:51:49 -0500

    Se crea ServicioRemision

---
+++ DemoRicomidas/Ricomidas.Core.Data/Services/ServicioRemision.cs
@@ -0,0 +1,37 @@
+﻿using Ricomidas.Core.Data.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ricomidas.Core.Data.Services
+{
+    public class ServicioRemision : IService<Remision>
+    {
+        public void Delete(Remision element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Remision> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Remision GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Remision element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Remision element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
